1cc2ae8ee85151ae82b996aab49f345afb39a756
Joshua Gray <joshgray91@gmail.com>
2025-04-16 16:23:56 +0100

    write unique outward codes to csv file

--- NI_postcodes.py
+++ NI_postcodes.py
@@ -1,16 +1,27 @@
 import csv
 
-csv_file = 'ONSPD_FEB_2024_UK_BT.csv'
+input_file = 'ONSPD_FEB_2024_UK_BT.csv'
+output_file = 'unique_ni_codes.csv'
+
 
 pcds_values = []
 
-with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
+with open(input_file, mode='r', newline='', encoding='utf-8') as infile, \
+     open(output_file, mode='w', newline='', encoding='utf-8') as outfile:   
+     
+    reader = csv.DictReader(infile)
+    writer = csv.writer(outfile)
+
+    writer.writerow(['outward_code'])
+
+    prev_value = None
+
+
     for row in reader:
         full_value = row['pcds']
         before_space = full_value.split(' ')[0] 
 
-        if not pcds_values or before_space != pcds_values[-1]:
-            pcds_values.append(before_space)
+        if before_space != prev_value:
+            writer.writerow([before_space])
+            prev_value = before_space
 
-print(pcds_values)
